1df91c2627e27f58daaa427731538ea93062de23
Jack Bittiner <jackbittiner@hotmail.com>
2017-05-02 11:39:59 +0100

    Adding unique name validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
 
-  validates :name, length: { maximum: 30 }
+  validates :name, length: { maximum: 30 }, uniqueness: true
 
   has_many :reviews, dependent: :destroy
 
